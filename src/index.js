2ffb69b525e4838033cd1bfd933488440d59791f
jon <Jon.Bos@gofastpath.com>
2022-03-26 18:27:23 -0500

    emit child to parent

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import * as Vue from "vue/dist/vue.esm-bundler.js";
 const Num = {
   template: `
-    <div     v-bind:class="getClass(number)" >
+    <button     
+      v-bind:class="getClass(number)" 
+      v-on:click="click"
+      >
     {{number}}
-    </div>
+    </button>
     `,
   props: ["number"],
   methods: {
+    click(n) {
+      this.$emit("chosen", this.number);
+      console.log(this.number);
+    },
     isEven(val) {
       return val % 2 == 0;
     },
     getClass(number) {
       return this.isEven(number) ? "blue" : "red";
     },
-
-  } 
+  },
 };
 
 const app = Vue.createApp({
@@ -24,53 +30,33 @@ const app = Vue.createApp({
   template: `
   <div>
 
-  <hello greeting="Hello"/>
-  <button v-on:click="increment">Increment</button>
-  <p> {{ count }} </p>
-  <div 
-    v-for="number in numbers"> 
-      <num v-bind:number="number" />
+  <num 
+    v-bind:number="number" 
+    v-for="number in numbers"
+    v-on:chosen="addNumber"
+    />
   </div>
-  <input
-    v-model="value"
+  <hr/>
+  <num 
+    v-bind:number="number" 
+    v-for="number in chosenNumbers"
+    v-on:chosen="addNumber"
   />
-  <div class='red'>
-    {{error}}
-  </div>
-  {{value}}
-  <input
-  type="checkbox"
-  v-model="value"
-  value="a"
-/>
-<input
-  type="checkbox"
-  v-model="value"
-  value="b"
-/>
-
-  </div>
   `,
   data() {
     return {
-      count: 0,
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      value: ["a"],
+      chosenNumbers: []
     };
   },
   methods: {
-    increment() {
-      this.count += 1;
-    },
+    addNumber(num) {
+      this.chosenNumbers.push(num)
+    }
   },
   computed: {
     evenList() {
       return this.numbers.filter((x) => this.isEven(x));
-    },
-    error() {
-      if (this.value.length < 5) {
-        return "Must be 5 characters";
-      }
     },
   },
 });
