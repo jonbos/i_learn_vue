3bc400c6a1721e713e372adc4cb413fbd85060a6
jon <Jon.Bos@gofastpath.com>
2022-03-27 18:20:26 -0500

    demonstrate reactive

--- composition/App.vue
+++ composition/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div>
     <button @click="increment">{{ count }}</button>
+    <button @click="increase('a')">{{ numbers.a }}</button>
+    <button @click="increase('b')">{{ numbers.b }}</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 export default {
   setup() {
+    // ref good for number or string
     const count = ref(0);
+
+    // reactive good for obj
+    const numbers = reactive({ a: 0, b: 0 });
 
     const increment = () => {
       count.value += 1;
     };
-    return { count, increment };
+    const increase = (n) => {
+      numbers[n] += 1;
+    };
+    return {
+      count,
+      increment,
+      numbers,
+      increase,
+    };
   },
 };
 </script>
